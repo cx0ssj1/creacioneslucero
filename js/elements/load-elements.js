a3208b79e8133e98c2a58f42e0e29278be3d1d02
cx0ssj1 <cx0ssj453@gmail.com>
2025-05-06 10:41:04 -0400

    #optimizacion code 2

--- js/elements/load-elements.js
+++ js/elements/load-elements.js
@@ -1,4 +1,14 @@
-fetch("/html/elements/navbar.html")
+fetch("/html/elements/footer.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("footer-container").innerHTML = data;
+        setTimeout(() => {
+            inicializarLogin();
+        }, 300);
+    })
+    .catch(error => console.error("Error al cargar el footer:", error));
+
+    fetch("/html/elements/navbar.html")
     .then(response => {
         if (!response.ok) {
             throw new Error(`Error HTTP: ${response.status}`);
--- js/elements/load-footer.js
+++
@@ -1,9 +0,0 @@
-fetch("/html/elements/footer.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("footer-container").innerHTML = data;
-        setTimeout(() => {
-            inicializarLogin();
-        }, 300);
-    })
-    .catch(error => console.error("Error al cargar el footer:", error));
